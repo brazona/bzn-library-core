2466af87c3c2fdba11dc065c42a941c028fbc12a
Cezar Silva <cezarfelipe2008@outlook.com>
2025-08-06 23:14:01 -0300

    feat: incluido esrtutura inicial para publicação do artefato

---
+++ app/src/main/java/br/brazona/bzn_library_core/BznLibraryCoreApplication.java
@@ -0,0 +1,13 @@
+package br.brazona.bzn_library_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BznLibraryCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BznLibraryCoreApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/br/brazona/bzn_library_core/BznLibraryCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.brazona.bzn_library_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BznLibraryCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
