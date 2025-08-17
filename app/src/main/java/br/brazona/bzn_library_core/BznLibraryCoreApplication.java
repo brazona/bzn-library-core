8c5f4fd720aa8a58f288bba47ce17dd98877fe80
Cezar Silva <cezarfelipe2008@outlook.com>
2025-08-17 20:05:05 -0300

    feat: removido inicialização spring da biblioteca

--- app/src/main/java/br/brazona/bzn_library_core/BznLibraryCoreApplication.java
+++ app/src/main/java/br/brazona/bzn_library_core/BznLibraryCoreApplication.java
@@ -3,7 +3,7 @@ package br.brazona.bzn_library_core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class BznLibraryCoreApplication {
 
 	public static void main(String[] args) {
