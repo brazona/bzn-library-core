f412f3878d609c91b3f5727c479aa8a53648ed6f
Cezar Silva <cezarfelipe2008@outlook.com>
2025-08-22 18:31:12 -0300

    feat: removido comentario classe ResponseModel

--- app/src/main/java/br/brazona/bzn_library_core/models/ResponseModel.java
+++ app/src/main/java/br/brazona/bzn_library_core/models/ResponseModel.java
@@ -6,21 +6,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * Modelo de resposta padrão utilizado para retornar informações sobre o status,
- * mensagem e data de processamento das operações da aplicação.
- *
- * <p>Inclui os campos:
- * <ul>
- *   <li>code - código de status da resposta</li>
- *   <li>message - mensagem associada à resposta</li>
- *   <li>dateProcessed - data em que a resposta foi processada</li>
- * </ul>
- *
- * @author Brazona Team
- */
-
-
 @Getter
 @Setter
 @Builder
